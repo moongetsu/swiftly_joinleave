0e2961baa5ee151d11ded7c56a568854f22e6cc5
Moongetsu <111883135+moongetsu@users.noreply.github.com>
2023-12-02 00:54:15 +0200

    Added the translation!

--- src/main.cpp
+++ src/main.cpp
@@ -17,22 +17,22 @@ void OnProgramLoad(const char *pluginName, const char *mainFilePath)
     config = new Configuration();
 }
 
-bool OnClientConnect(Player *player)
+bool OnClientConnect(Player *player)    
 {
     print(FetchTranslation("joinleave.join.message"), config->Fetch<const char*>("joinleave.prefix"), player->GetName());
-    logger->Write(LOGLEVEL_COMMON, "[%s] %s has connected on the server!", __DATE__, player->GetName());
+    logger->Write(LOGLEVEL_COMMON, "%s has connected on the server!", player->GetName());
     return true;
 }
 
 void OnClientDisconnect(Player *player)
 {
     print(FetchTranslation("joinleave.leave.message"), config->Fetch<const char*>("joinleave.prefix"), player->GetName());
-    logger->Write(LOGLEVEL_COMMON, "[%s] %s has disconnected from the server!", __DATE__, player->GetName());
+    logger->Write(LOGLEVEL_COMMON, "%s has disconnected from the server!", player->GetName());
 }
 
 void OnPluginStart()
 {
-    logger->Write(LOGLEVEL_COMMON, "[%s] The plugin started the log session.", __DATE__);
+    logger->Write(LOGLEVEL_COMMON, "The plugin started the log session.");
 }
 
 void OnPluginStop()
