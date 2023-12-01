6c42a08d4fc4a7185a46b1266c813b1accdd3a86
Moongetsu <111883135+moongetsu@users.noreply.github.com>
2023-12-01 23:47:23 +0200

    Let's hope this is the last Action xd

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include <swiftly/swiftly.h>
 #include <swiftly/server.h>
+#include <swiftly/logger.h>
 
 Server *server = nullptr;
 PlayerManager *g_playerManager = nullptr;
+Logger *logger = nullptr;
 
 void OnProgramLoad(const char *pluginName, const char *mainFilePath)
 {
     Swiftly_Setup(pluginName, mainFilePath);
     g_playerManager = new PlayerManager();
     server = new Server();
+    logger = new Logger(pluginName, mainFilePath);
 }
 
 bool OnClientConnect(Player *player)
@@ -16,11 +19,13 @@ bool OnClientConnect(Player *player)
     print("------------------------------------------------ \n");
     print("%s s-a conectat pe server! \n", player->GetName());
     print("------------------------------------------------ \n");
+    logger->Write(LOGLEVEL_COMMON, "[%s] %s s-a conectat pe server!", __DATE__, player->GetName());
     return true;
 }
 
 void OnPluginStart()
 {
+    logger->Write(LOGLEVEL_COMMON, "[%s] Plugin-ul a pornit", __DATE__);
 }
 
 void OnPluginStop()
