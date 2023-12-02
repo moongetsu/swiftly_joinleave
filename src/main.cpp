163258adc26da3a00d81339d2a9e21b105a04a32
Moongetsu <111883135+moongetsu@users.noreply.github.com>
2023-12-02 12:11:00 +0200

    Added debug

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@ void OnProgramLoad(const char *pluginName, const char *mainFilePath)
 bool OnClientConnect(Player *player)    
 {
     g_playerManager->SendMsg(HUD_PRINTTALK, FetchTranslation("joinleave.join.message"), config->Fetch<const char*>("joinleave.prefix"), player->GetName());
+    print(FetchTranslation("joinleave.join.message"), config->Fetch<const char*>("joinleave.prefix"), player->GetName());
     logger->Write(LOGLEVEL_COMMON, "%s has connected on the server!", player->GetName());
     return true;    
 }
@@ -27,6 +28,7 @@ bool OnClientConnect(Player *player)
 void OnClientDisconnect(Player *player)
 {
     g_playerManager->SendMsg(HUD_PRINTTALK, FetchTranslation("joinleave.leave.message"), config->Fetch<const char*>("joinleave.prefix"), player->GetName());
+    print(FetchTranslation("joinleave.leave.message"), config->Fetch<const char*>("joinleave.prefix"), player->GetName());
     logger->Write(LOGLEVEL_COMMON, "%s has disconnected from the server!", player->GetName());
 }
 
